3286e5da6fd4859a7e70742bd251f7a35af3d989
koidzt <koi27792@gmail.com>
2020-09-17 23:00:26 +0700

    add Homework JavaScript 16-Sep-20

---
+++ JavaScript/index.js
@@ -0,0 +1,84 @@
+// ข้อ 1
+// let message = '',n = +prompt('Please enter number for n','');
+// for(let i = 0; i < n ; i++) message = message + '*';
+// alert(message);
+
+// ข้อ 2
+// let message='',n = +prompt('Please enter number for n','');
+// for(let i = 0; i < n ; i++) {
+//     message = message + '\n';
+//     for(let j = 0; j < n ; j++) message = message + '*';
+// }
+// alert(message);
+
+// ข้อ 3
+// let message='',n = +prompt('Please enter number for n','');
+// for(let i = 0; i < n ; i++) {
+//     message = message + '\n';
+//     for(let j = 1; j <= n ; j++) message = message + j;
+// }
+// alert(message);
+
+// ข้อ 4
+// let message='',n = +prompt('Please enter number for n','');
+// for(let i = 1; i <= n ; i++) {
+//     for(let j = 1; j <= n ; j++) message = message + `${i}`;
+//     message = message + '\n';
+// }
+// alert(message);
+
+// ข้อ 5
+// let message='',n = +prompt('Please enter number for n','');
+// for(let i = n; i >= 1 ; i--) {
+//     for(let j = 1; j <= n ; j++) message = message + `${i}`;
+//     message = message + '\n';
+// }
+// alert(message);
+
+// ข้อ 6
+// let message='',n = +prompt('Please enter number for n','');
+// let c = 1;
+// for(let i = 1; i <= n; i++) {
+//     for (let j = 1 ; j <= n; j++) {
+//         message = message + `${c}`;
+//         c++;
+//     }
+//     message = message + '\n';
+// }
+// alert(message);
+
+// ข้อ 7
+
+// ข้อ 8
+
+// ข้อ 9
+
+// ข้อ 10
+
+// ข้อ 11
+
+// ข้อ 12
+
+// ข้อ 13
+
+// ข้อ 14
+
+// ข้อ 15
+
+// ข้อ 16
+
+// ข้อ 17
+
+// ข้อ 18
+
+// ข้อ 19
+
+// ข้อ 20
+
+// ข้อ 21
+
+// ข้อ 22
+
+// ข้อ 23
+
+// ข้อ 24
